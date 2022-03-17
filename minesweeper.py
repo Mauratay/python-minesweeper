d3739773f4a52991a421d259c73c1a73a21bb69f
mauratay <francisco.mauratay@gmail.com>
2022-03-16 19:03:11 -0600

    description

---
+++ minesweeper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Deadline:
+Friday
+
+Pseudo Code
+
+1 make square 25 x 25 asterisc long
+2 each asterisc is a random number
+3 limit random number from 1 to 10
+4 each try the random number is reduced by 2 until reaching one and then back to 10
+5 the asteriscs around the one picked are to be taken out
+6 random asteriscs are mines, if one's hit, game over
+7 
+"""
