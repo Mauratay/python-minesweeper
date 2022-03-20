da47b8c7b80f2b58c6a80d3dc04bfb8c3503728e
mauratay <francisco.mauratay@gmail.com>
2022-03-20 12:16:05 -0600

    update

--- minesweeper.py
+++ minesweeper.py
@@ -7,7 +7,7 @@ Friday
 Pseudo Code
 
 1 make square 25 x 25 asterisc long
-2 each asterisc is a random number
+2 each asterisc is a random number     
 3 limit random number from 1 to 10
 4 each try the random number is reduced by 2 until reaching one and then back to 10
 5 the asteriscs around the one picked are to be taken out
@@ -19,25 +19,34 @@ import random
 
 class Asterisc:
 
-    def map():
-        blueprint = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    blueprint = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    for i in range(19):
+        new = blueprint
+        new[i].append('*')
         for i in range(19):
             new = blueprint
             new[i].append('*')
+        
+
+    def reset(self):
+        for i in range(19):
+            new = self.blueprint
+            new[i].append('*')
             for i in range(19):
-                new = blueprint
+                new = self.blueprint
                 new[i].append('*')
-        return blueprint
+        return self.blueprint
 
     def random_number(x):
         result = random.randrange(0,x,2)
         return result
+# --------------------------------------
 
-
-
-def background(x,y):
-    star = Asterisc.map()
-    star[x][y]='X'
+def background(y,x):
+    star = Asterisc.blueprint
+    y = int(y)
+    x = int(x)
+    star[y][x]= 'X'
     print("""
 ███╗░░░███╗██╗███╗░░██╗███████╗░██████╗░██╗░░░░░░░██╗███████╗███████╗██████╗░███████╗██████╗░
 ████╗░████║██║████╗░██║██╔════╝██╔════╝░██║░░██╗░░██║██╔════╝██╔════╝██╔══██╗██╔════╝██╔══██╗
@@ -55,7 +64,35 @@ def background(x,y):
     """)
 
 
-x = input("Select by coordinate X: ")
-y = input("Select by coordinate Y: ")
+def verifier(num):
+    num = int(num)
+    if num > 20:
+        raise Exception("Error number should be smaller than 19")
+    elif num < 0:
+        raise Exception("Error number should be bigger than 0")
+    else:
+        return num
 
-background(x,y)
+def shot():
+    y = input("Select by coordinate Y: (from 0 to 19)\n")
+    y = verifier(y)
+    x = input("Select by coordinate X: (from 0 to 19)\n")
+    x = verifier(x)
+    background(y,x)
+
+again = True
+
+shot()
+
+while again == True:
+    ans = input("Another one? [y] [n]\n")
+    if ans == 'y':
+        shot()
+    elif ans == 'n':
+        print("Goodbye!")
+        break
+
+
+
+
+
